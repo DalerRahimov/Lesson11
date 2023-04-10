eb6900c18742cd2cd4ee58cd937aab1fd61fb021
DalerRahimov <drahimov640@gmail.com>
2023-04-10 16:23:38 +0500

    map

---
+++ Lists/Lists.go
@@ -0,0 +1,20 @@
+package Lists
+
+var CardList1 = []string{"0213-2321", "4215-2321", "5421-2321", "2245-2321", "9865-2321"}
+var CardList2 = []string{"0000-2321", "3211-2321", "8213-2321", "9213-2321", "7213-2321"}
+
+func Sort() (HumoMap, OthersBanksMap map[string]string) {
+	TotalList := append(CardList1, CardList2...)
+	HumoMap = make(map[string]string)
+	OthersBanksMap = make(map[string]string)
+	for _, v := range TotalList {
+		if v[0] >= 53 {
+			HumoMap[v] = ""
+
+		} else {
+			OthersBanksMap[v] = ""
+		}
+	}
+
+	return
+}
